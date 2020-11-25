f9ae19588f0af0e86483d7a8cf4718afe1f5fd32
Sinn <linsinn@outlook.com>
2020-11-25 23:40:43 +0800

    add vec3

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use crate::vec3::Color;
+
+mod vec3;
+mod color;
+
+fn main() {
+	let image_width = 256;
+	let image_height = 256;
+
+	println!("P3\n{} {}\n255", image_width, image_height);
+
+	for i in (0..image_height).rev() {
+		eprint!("\rScan lines remaining: {}", i);
+		for j in 0..image_width {
+			let color = Color::from_slice(
+				&[j as f64 / (image_width - 1) as f64, i as f64 / (image_height - 1) as f64, 0.25]
+			);
+			color::write_color(color);
+		}
+	}
+}
---
+++ src/vec3.rs
@@ -0,0 +1,143 @@
+use std::ops;
+use std::convert::{TryFrom, TryInto};
+use std::ops::{Add, Neg, Sub, Mul};
+
+#[derive(Copy, Clone, Debug)]
+pub struct Vec3 {
+	pub e: [f64; 3]
+}
+
+impl Vec3 {
+	pub fn new() -> Self {
+		Self {
+			e: [0.0; 3]
+		}
+	}
+
+	pub fn from_slice(arr: &[f64]) -> Self {
+		Self {
+			e: <[f64; 3]>::try_from(arr).unwrap()
+		}
+	}
+
+	pub fn x(&self) -> f64 { self.e[0] }
+	pub fn y(&self) -> f64 { self.e[1] }
+	pub fn z(&self) -> f64 { self.e[2] }
+
+	pub fn length(&self) -> f64 {
+		self.length_squared().sqrt()
+	}
+
+	pub fn length_squared(&self) -> f64 {
+		self.e.iter().map(|&v| v * v).sum::<f64>()
+	}
+}
+
+impl ops::Neg for Vec3 {
+	type Output = Self;
+	fn neg(self) -> Self::Output {
+		Self {
+			e: self.e.iter().map(|&v| -v).collect::<Vec<f64>>().try_into().unwrap()
+		}
+	}
+}
+
+impl ops::Sub for Vec3 {
+	type Output = Self;
+	fn sub(self, rhs: Self) -> Self::Output {
+		Self {
+			e: self.e.iter().zip(rhs.e.iter()).map( | ( & a, &b) | a - b).collect:: < Vec<f64> > ().try_into().unwrap()
+		}
+	}
+}
+
+impl ops::AddAssign for Vec3 {
+	fn add_assign(&mut self, rhs: Vec3) {
+		for (i, &v) in rhs.e.iter().enumerate() {
+			self.e[i] += v;
+		}
+	}
+}
+
+impl ops::Add for Vec3 {
+	type Output = Self;
+	fn add(self, rhs: Self) -> Self::Output {
+		Self {
+			e: self.e.iter().zip(rhs.e.iter()).map( | ( & a, &b) | a + b).collect:: < Vec<f64> > ().try_into().unwrap()
+		}
+	}
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+	fn mul_assign(&mut self, rhs: f64) {
+		for v in self.e.iter_mut() {
+			*v *= rhs;
+		}
+	}
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+	type Output = Self;
+	fn mul(self, rhs: Vec3) -> Self::Output {
+		Self {
+			e: self.e.iter().zip(rhs.e.iter()).map( | ( & a, &b) | a * b).collect:: < Vec<f64> > ().try_into().unwrap()
+		}
+	}
+}
+
+
+impl ops::Mul<f64> for Vec3 {
+	type Output = Self;
+	fn mul(self, rhs: f64) -> Self::Output {
+		Self {
+			e: self.e.iter().map( |&a| a * rhs).collect:: < Vec<f64> > ().try_into().unwrap()
+		}
+	}
+}
+
+impl ops::Mul<Vec3> for f64 {
+	type Output = Vec3;
+	fn mul(self, rhs: Vec3) -> Self::Output {
+		Self::Output {
+			e: rhs.e.iter().map( |&a| a * self).collect:: < Vec<f64> > ().try_into().unwrap()
+		}
+	}
+}
+
+impl ops::DivAssign<f64> for Vec3 {
+	fn div_assign(&mut self, rhs: f64) {
+		for v in self.e.iter_mut() {
+			*v /= rhs;
+		}
+	}
+}
+
+impl ops::Div<f64> for Vec3 {
+	type Output = Self;
+	fn div(self, rhs: f64) -> Self::Output {
+		Self {
+			e: self.e.iter().map( |&a| a / rhs).collect:: < Vec<f64> > ().try_into().unwrap()
+		}
+	}
+}
+
+#[inline]
+pub fn dot(u: Vec3, v: Vec3) -> f64{
+	u.e.iter().zip(v.e.iter()).map(|(&a, &b)| a * b).sum::<f64>()
+}
+
+#[inline]
+pub fn cross(u: Vec3, v: Vec3) -> Vec3 {
+	let mut res = Vec3::new();
+	res.e[0] = u.e[1] * v.e[2] - u.e[2] * v.e[1];
+	res.e[1] = u.e[2] * v.e[0] - u.e[0] * v.e[2];
+	res.e[2] = u.e[0] * v.e[1] - u.e[1] * v.e[0];
+	res
+}
+
+pub fn unit_vector(v: Vec3) -> Vec3 {
+	v / v.length()
+}
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
