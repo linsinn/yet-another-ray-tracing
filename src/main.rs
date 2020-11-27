097f25aa29bc926c840ad0e2cddbff47a0ccfc22
Sinn <linsinn@outlook.com>
2020-11-27 23:57:34 +0800

    Shading with Surface Normals

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,26 @@ mod vec3;
 mod color;
 mod ray;
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
 	let oc = r.origin() - center;
 	let a = dot(r.direction(), r.direction());
 	let b = 2.0 * dot(oc, r.direction());
 	let c = dot(oc, oc) - radius * radius;
 	let discriminant = b * b - 4.0 * a * c;
-	discriminant > 0.0
+	if discriminant < 0.0 {
+		-1.0
+	} else {
+		(-b - discriminant.sqrt()) / (2.0 * a)
+	}
 }
 
 fn ray_color(r: &Ray) -> Color {
-	if hit_sphere(Point3::new(0, 0, 1), 0.5, *r) {
-		return Color::new(1, 0, 0);
+	let t = hit_sphere(Point3::new(0, 0, -1), 0.5, *r);
+	if t > 0.0 {
+		let n = unit_vector(r.at(t) - Vec3::new(0, 0, -1));
+		return Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
 	}
+
 	let unit_direction = unit_vector(r.direction());
 	let t = 0.5 * (unit_direction.y() + 1.0);
 	(1.0 - t) * Color::new(1, 1, 1) + t * Color::new(0.5, 0.7, 1.0)
