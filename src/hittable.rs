8763437af039b357178f6909956644f81e200eeb
Sinn <linsinn@outlook.com>
2020-11-28 17:28:34 +0800

    Front Faces Versus Back Faces

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,10 +1,23 @@
-use crate::vec3::{Point3, Vec3};
+use crate::vec3::{Point3, Vec3, dot};
 use crate::ray::Ray;
 
 pub struct HitRecord {
 	pub p: Point3,
 	pub normal: Vec3,
 	pub t: f64,
+	pub front_face: bool,
+}
+
+impl HitRecord {
+	#[inline]
+	pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+		self.front_face = dot(r.direction(), *outward_normal) < 0.0;
+		self.normal = if self.front_face {
+			*outward_normal
+		} else {
+			*outward_normal
+		}
+	}
 }
 
 pub trait Hittable {
