f027c9ea1b6c72da6ad511df1e358b7c47cb63c7
Sinn <linsinn@outlook.com>
2020-12-02 17:38:41 +0800

    finally

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crate::vec3::{Color, Vec3, unit_vector, Point3, dot, random_unit_vector, ran
 use crate::ray::Ray;
 use crate::color::get_pixel_color;
 use crate::hittable::{Hittable, HitRecord};
-use crate::utils::{INFINITY, random_double};
+use crate::utils::{INFINITY, random_double, random_double_range};
 use crate::hittable_list::HittableList;
 use std::rc::Rc;
 use crate::sphere::Sphere;
@@ -21,6 +21,7 @@ mod material;
 use image;
 use std::cmp::max;
 use crate::material::{Lambertian, Metal, Dielectric};
+use rand::random;
 
 fn ray_color<T: Hittable>(r: &Ray, world: &T, depth: i32) -> Color {
 	let mut rec = HitRecord::new();
@@ -52,32 +53,21 @@ fn ray_color<T: Hittable>(r: &Ray, world: &T, depth: i32) -> Color {
 
 fn main() {
 	// Image
-	let aspect_ratio = 16.0 / 9.0;
-	let image_width = 400;
+	let aspect_ratio = 3.0 / 2.0;
+	let image_width = 900;
 	let image_height = (image_width as f64 / aspect_ratio) as i32;
-	let samples_per_pixel = 100;
+	let samples_per_pixel = 500;
 	let max_depth = 50;
 
 	// World
-	let mut world = HittableList::new();
-
-	let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-	let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-	let material_left = Rc::new(Dielectric::new(1.5));
-	let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
-
-	world.add(Rc::new(Sphere::new(Point3::new(0, -100.5, -1), 100, material_ground)));
-	world.add(Rc::new(Sphere::new(Point3::new(0, 0, -1), 0.5, material_center)));
-	world.add(Rc::new(Sphere::new(Point3::new(-1, 0, -1), 0.5, material_left.clone())));
-	world.add(Rc::new(Sphere::new(Point3::new(-1, 0, -1), -0.45, material_left)));
-	world.add(Rc::new(Sphere::new(Point3::new(1, 0, -1), 0.5, material_right)));
+	let world = random_scene();
 
 	// Camera
-	let look_from = Point3::new(3, 3, 2);
-	let look_at = Point3::new(0, 0, -1);
+	let look_from = Point3::new(13, 2, 3);
+	let look_at = Point3::new(0, 0, 0);
 	let v_up = Vec3::new(0, 1, 0);
-	let dist_to_focus = (look_from - look_at).length();
-	let aperture = 10.0;
+	let dist_to_focus = 10.0;
+	let aperture = 0.1;
 
 	let cam = Camera::new(
 		look_from,
@@ -109,4 +99,44 @@ fn main() {
 	}
 	image::save_buffer("image.png", &buf, image_width as u32, image_height as u32, image::ColorType::Rgb8).unwrap();
 	eprint!("\nDone.\n");
+}
+
+fn random_scene() -> HittableList<Sphere> {
+	let mut world = HittableList::new();
+	let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+	world.add(Rc::new(Sphere::new(Point3::new(0, -1000, 0), 1000.0, ground_material)));
+	for a in -11..11 {
+		for b in -11..11 {
+			let choose_mat = random_double();
+			let center = Point3::new(a as f64 + 0.9 * random_double(), 0.2, b as f64 + 0.9 * random_double());
+
+			if (center - Point3::new(4, 0.2, 0)).length() > 0.9 {
+				if choose_mat < 0.8 {
+					// diffuse
+					let albedo = Color::random() * Color::random();
+					let sphere_material = Rc::new(Lambertian::new(albedo));
+					world.add(Rc::new(Sphere::new(center.clone(), 0.2, sphere_material)));
+				} else if choose_mat < 0.95 {
+					// metal
+					let albedo = Color::random_range(0.5, 1.0);
+					let fuzz = random_double_range(0.0, 0.5);
+					let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+					world.add(Rc::new(Sphere::new(center.clone(), 0.2, sphere_material)));
+				} else {
+					// glass
+					let sphere_material = Rc::new(Dielectric::new(1.5));
+					world.add(Rc::new(Sphere::new(center.clone(), 0.2, sphere_material)));
+				}
+			}
+		}
+	}
+	let material = Rc::new(Dielectric::new(1.5));
+	world.add(Rc::new(Sphere::new(Point3::new(0, 1, 0), 1.0, material)));
+
+	let material = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+	world.add(Rc::new(Sphere::new(Point3::new(-4, 1, 0), 1.0, material)));
+
+	let material = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+	world.add(Rc::new(Sphere::new(Point3::new(4, 1, 0), 1.0, material)));
+	world
 }
